d045ccaab75275369d4203ecbcc1c95457ccf573
Hannah Carnes <hecarnes@gmail.com>
2019-06-12 19:44:42 -0400

    render a single marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { latLng, tileLayer, marker, icon, Layer } from 'leaflet';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,8 +12,19 @@ export class AppComponent {
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
     ],
     zoom: 11,
-    center: latLng(40.758700379161006, -73.95652770996094),
+    center: latLng(40.758700379161006, -73.95652770996094)
   };
+
+  layers: Layer[] = [
+    marker([ 40.758700379161006, -73.95652770996094 ], {
+      icon: icon({
+        iconSize: [ 25, 41 ],
+        iconAnchor: [ 13, 41 ],
+        iconUrl: 'leaflet/marker-icon.png',
+        shadowUrl: 'leaflet/marker-shadow.png'
+      })
+    })
+  ];
 
   title = 'nyc-wifi';
 }
