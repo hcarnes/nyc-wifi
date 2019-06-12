e8fe74298375f2bbb0c2a8a0a7fdafb3f33bb808
Hannah Carnes <hecarnes@gmail.com>
2019-06-12 17:30:15 -0400

    Render map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { latLng, tileLayer } from 'leaflet';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
+
   title = 'nyc-wifi';
 }
